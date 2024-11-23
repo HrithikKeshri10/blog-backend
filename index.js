4c9c0dd6bd689675ec1f7ae9feb4c24445b70855
HrithikKeshri10 <hrithikkeshri10@gmail.com>
2024-11-23 22:09:20 +0530

    cors changes

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: "https://blog-frontend-nine-kappa.vercel.app",
     credentials: true,
+    optionsSuccessStatus: 200,
   })
 );
 
