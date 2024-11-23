d8af11a66dadb9d4891318d52d7434187b2b31d1
HrithikKeshri10 <hrithikkeshri10@gmail.com>
2024-11-23 21:28:22 +0530

    cors issue fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://blog-frontend-lcat.vercel.app/",
+    origin: "https://blog-frontend-nine-kappa.vercel.app",
     credentials: true,
   })
 );
